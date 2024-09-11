6cca711149245f801d57d22e1e6a9817ba92b1cc
KavyaC11 <kmoments11@gmail.com>
2024-09-11 15:14:33 +0530

    contests

---
+++ src/fakeDatabase.js
@@ -0,0 +1,8 @@
+// src/fakeDatabase.js
+export const contestsData = [
+    { id: 1, title: "Contest 1", details: "Details for Contest 1" },
+    { id: 2, title: "Contest 2", details: "Details for Contest 2" },
+    { id: 3, title: "Contest 3", details: "Details for Contest 3" },
+    { id: 4, title: "Contest 4", details: "Details for Contest 4" },
+  ];
+  
