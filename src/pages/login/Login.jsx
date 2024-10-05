2eb89717a05e44eb328a19a3806b9e11b51cb275
Kedar <kedarkisan2020@outlook.com>
2024-10-06 00:03:39 +0530

    login page added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,31 @@
 import styles from './login.module.css'
 
 const Login = () => {
-	return(
+	return (
 		<div className={styles.example}>
-			Login
+			<div className="container">
+				<div className="form">
+					<form action="#">
+						<label htmlFor="Primary_email">Primary Email</label>
+						<br />
+						<input type="text" placeholder="Value" />
+						<br />
+						<label htmlFor="Password">Password</label>
+						<br />
+						<input type="text" placeholder="Value" />
+						<br />
+						<button className="login">Log-in</button>
+						<br />
+						<a href="#">Forgot Password?</a>
+						<div className="or">
+							<hr />
+							<p style={{ position: "relative", top: "-22px" }}>or</p>
+							<hr />
+						</div>
+						<button className="google_login"><FcGoogle />  Continue With Google</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 }
