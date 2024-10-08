e9790b83ff901c9c642f215eea60ada748ed8b53
Kedar <kedarkisan2020@outlook.com>
2024-10-08 22:14:47 +0530

    few errors in login page resolved

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,11 @@
+import React, { useState } from 'react';
 import styles from './login.module.css'
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
 	return (
 		<div className={styles.example}>
 			<div className="container">
@@ -8,11 +13,11 @@ const Login = () => {
 					<form action="#">
 						<label htmlFor="Primary_email">Primary Email</label>
 						<br />
-						<input type="text" placeholder="Value" />
+						<input type="email" placeholder="Value" value={email} onChange={(e) => setEmail(e.target.value)} required/>
 						<br />
 						<label htmlFor="Password">Password</label>
 						<br />
-						<input type="text" placeholder="Value" />
+						<input type="password" placeholder="Value" value={password} onChange={(e) => setPassword(e.target.value)} required/>
 						<br />
 						<button className="login">Log-in</button>
 						<br />
