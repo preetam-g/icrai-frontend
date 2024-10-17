c2a37d53d9b7821c4a25b7b23bfe56527d1f06c2
Preetam G <118665778+preetam-g@users.noreply.github.com>
2024-10-17 20:56:22 +0530

    color theme changed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from './login.module.css'
 import { FcGoogle } from "react-icons/fc";
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 	const [email, setEmail] = useState('');
@@ -13,22 +14,23 @@ const Login = () => {
 					<form action="#">
 						<label htmlFor="Primary_email">Primary Email</label>
 						<br />
-						<input type="email" placeholder="Value" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+						<input type="email" placeholder="abc@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
 						<br />
 						<label htmlFor="Password">Password</label>
 						<br />
-						<input type="password" placeholder="Value" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+						<input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
 						<br />
 						<button className={styles.login}>Log-in</button>
 						<br />
-						<a href="#">Forgot Password?</a>
+						<a href="#">Forgot Password?</a> {/* modify link to enable password reset mechanism */}
 						<div className={styles.or}>
 							<hr />
 							<p style={{ position: "relative", top: "-22px" }}>or</p>
 							<hr />
 						</div>
-						<button className={styles.google_login}><FcGoogle />  Continue With Google</button>
+						<button className={styles.google_login}> <FcGoogle />  Continue With Google </button>
 					</form>
+					<Link to="/Register" className={styles.registerLink}>Don't have an account? <br /> Register Here</Link>
 				</div>
 			</div>
 		</div>
