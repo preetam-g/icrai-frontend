7bb45e8235e4c9512e9e30c53d548dccf7d39d9f
Preetam G <118665778+preetam-g@users.noreply.github.com>
2024-10-18 20:16:50 +0530

    updated loginPage

---
+++ src/components/general/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import styles from './navbar.module.css';
+import { Link, useLocation } from 'react-router-dom';
+import {navItems} from '../../fakeDatabase.js'
+
+const Navbar = ({selectedOption,setSelected}) => {
+
+    const [isShowingOptions,setShowingOptions] = useState(false);
+    const loc = useLocation();
+
+    useEffect(() => {
+        console.log(loc);
+        const currIndex = navItems.findIndex(item => item.path === location.pathname);
+        setSelected(currIndex);
+    }, [loc])
+
+    const handleClick=(index)=>{
+        setSelected(index);
+        setShowingOptions(false);
+    }
+    
+    const handleShowing=()=>{
+        isShowingOptions==true?setShowingOptions(false):setShowingOptions(true);
+    }
+
+    return (
+        <>
+        <div className={styles.navbar}>
+            <Link to='/' onClick={()=>handleClick(-1)}><span className={styles.Badge}>ICRAI</span></Link>
+            <div className={isShowingOptions ? styles.menushowing : styles.menuhiding}>
+                {navItems.map((item, idx) => (
+                    <Link
+                        key={idx}
+                        to={item.path}
+                        onClick={() => handleClick(idx)}
+                        className={`
+                            ${styles.options}
+                            ${selectedOption === idx ? styles.selected : styles.normal}
+                        `}
+                    >
+                    <span className={styles.navOptions}>{item.label}</span>
+                    </Link>
+                ))}
+            </div>
+        <div>
+            <button onClick={handleShowing} className={styles.dropdownBtn}>{isShowingOptions==false?'Menu':'Close'}</button>
+        </div>
+        </div>
+        </>
+    );
+};
+
+export default Navbar;
