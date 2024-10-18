1ab590bf906790bc457cf7c8920bf73d1205408e
Preetam G <118665778+preetam-g@users.noreply.github.com>
2024-10-18 17:50:02 +0530

    updated Register Page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -19,65 +19,67 @@ const Register = () => {
 	return(
 		<div className={styles.example}>
 			<div className={styles.container}>
-				<form action='#' className={styles.form}>
-					<div className={styles.row} style={{margin: "3vh 0 auto"}}>
-						<div className={styles.column}>
-							<label htmlFor="First-Name">First Name*</label>
-							<input type="text" placeholder='Rage' value={firstName} onChange={e => setFirstName(e.target.value)} required />
+				<div className={styles.form}>
+					<form action='#' className={styles.form}>
+						<div className={styles.row} style={{margin: "3vh 0 auto"}}>
+							<div className={styles.column}>
+								<label htmlFor="First-Name">First Name*</label>
+								<input type="text" placeholder='Rage' value={firstName} onChange={e => setFirstName(e.target.value)} required />
+							</div>
+							<div className={styles.column}>
+								<label htmlFor="Last-Name">Last Name*</label>
+								<input type="text" placeholder='Titan' value={lastName} onChange={e => setLastName(e.target.value)} required />
+							</div>
 						</div>
-						<div className={styles.column}>
-							<label htmlFor="Last-Name">Last Name*</label>
-							<input type="text" placeholder='Titan' value={lastName} onChange={e => setLastName(e.target.value)} required />
+						<div className={styles.row}>
+							<div className={styles.column}>
+								<label htmlFor="university">University/College/School*</label>
+								<input type="text" placeholder='IIT Guwahati' value={university} onChange={e => setUniversity(e.target.value)} required />
+							</div>
+							<div className={styles.column}>
+								<label htmlFor="Age">Age*</label>
+								<input type="number" placeholder='23' value={age} onChange={e => setAge(e.target.value)} required />
+							</div>
 						</div>
-					</div>
-					<div className={styles.row}>
-						<div className={styles.column}>
-							<label htmlFor="university">University/College/School*</label>
-							<input type="text" placeholder='IIT Guwahati' value={university} onChange={e => setUniversity(e.target.value)} required />
+						<div className={styles.row}>
+							<div className={styles.column}>
+								<label htmlFor="number">Phone Number*</label>
+								<input type="number" placeholder='9876543210' value={number} onChange={e => setNumber(e.target.value)} required />
+							</div>
+							<div className={styles.column}>
+								<label htmlFor="altNumber">Alternate Number</label>
+								<input type="number" placeholder='9876543210' value={altNumber} onChange={e => setAltNumber(e.target.value)} />
+							</div>
 						</div>
-						<div className={styles.column}>
-							<label htmlFor="Age">Age*</label>
-							<input type="number" placeholder='23' value={age} onChange={e => setAge(e.target.value)} required />
+						<div className={styles.row}>
+							<div className={styles.column}>
+								<label htmlFor="number">Personal Email*</label>
+								<input type="text" placeholder='abc@gmail.com' value={email} onChange={e => setEmail(e.target.value)} required />
+							</div>
+							<div className={styles.column}>
+								<label htmlFor="altEmail">Alternate Email</label>
+								<input type="text" placeholder='abc@iitg.ac.in' value={altEmail} onChange={e => setAltEmail(e.target.value)} />
+							</div>
 						</div>
-					</div>
-					<div className={styles.row}>
-						<div className={styles.column}>
-							<label htmlFor="number">Phone Number*</label>
-							<input type="text" placeholder='9876543210' value={number} onChange={e => setNumber(e.target.value)} required />
+						<div className={styles.row}>
+							<div className={styles.column}>
+								<label htmlFor="number">Password*</label>
+								<input type="password" placeholder='******' value={password} onChange={e => setPassword(e.target.value)} required />
+							</div>
+							<div className={styles.column}>
+								<label htmlFor="password">Confirm Password*</label>
+								<input type="password" placeholder='******' value={cnfPassword} onChange={e => setCnfPassword(e.target.value)} required />
+							</div>
 						</div>
-						<div className={styles.column}>
-							<label htmlFor="altNumber">Alternate Number</label>
-							<input type="text" placeholder='9876543210' value={altNumber} onChange={e => setAltNumber(e.target.value)} />
+						<button className={styles.signUp} onClick={() => {}}>Sign-Up</button>
+						<div className={styles.or}>
+							<hr />
+							<p style={{ position: "relative", top: "-18px", verticalAlign: "middle" }}>or</p>
+							<hr />
 						</div>
-					</div>
-					<div className={styles.row}>
-						<div className={styles.column}>
-							<label htmlFor="number">Personal Email*</label>
-							<input type="text" placeholder='abc@gmail.com' value={email} onChange={e => setEmail(e.target.value)} required />
-						</div>
-						<div className={styles.column}>
-							<label htmlFor="altEmail">Alternate Email</label>
-							<input type="text" placeholder='abc@iitg.ac.in' value={altEmail} onChange={e => setAltEmail(e.target.value)} />
-						</div>
-					</div>
-					<div className={styles.row}>
-						<div className={styles.column}>
-							<label htmlFor="number">Password*</label>
-							<input type="password" placeholder='******' value={password} onChange={e => setPassword(e.target.value)} required />
-						</div>
-						<div className={styles.column}>
-							<label htmlFor="password">Confirm Password</label>
-							<input type="password" placeholder='******' value={cnfPassword} onChange={e => setCnfPassword(e.target.value)} />
-						</div>
-					</div>
-					<button className={styles.signUp} onClick={() => {}}>Sign-Up</button>
-					<div className={styles.or}>
-						<hr />
-						<p style={{ position: "relative", top: "-18px", verticalAlign: "middle" }}>or</p>
-						<hr />
-					</div>
+					</form>
 					<button className={styles.google_login} onClick={() => {}}> <FcGoogle />  Continue With Google </button>
-				</form>
+				</div>
 			</div>
 		</div>
 	);
